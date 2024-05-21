b8f2d27793868755f4902fdfc03c30fcd6642952
Chechurov Vladimir <vschechurov@edu.hse.ru>
2024-05-21 16:32:22 +0300

    1

--- src/main/kotlin/ru/hse/vlad/dao/TaskDaoImpl.kt
+++ src/main/kotlin/ru/hse/vlad/dao/TaskDaoImpl.kt
@@ -4,6 +4,7 @@ import ru.hse.vlad.enities.TaskEntity
 import java.sql.Connection
 import java.sql.DriverManager
 
+
 class TaskDaoImpl : TaskDao {
     val db : MutableList<TaskEntity> = mutableListOf()
     var connection : Connection? = null
@@ -11,25 +12,47 @@ class TaskDaoImpl : TaskDao {
         try {
             val user = "postgres"
             val password = "password"
-            Class.forName("org.postrgesql.Driver")
+            val table = "tasks"
+            Class.forName("org.postgresql.Driver")
             connection = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/",
+                "jdbc:postgresql://localhost:5432/postgres",
                 user, password
             )
-            if (connection != null) {
-                println("Connection established")
-            } else {
-                println("Connection failed")
+            if (connection == null) {
+                throw Exception("Connection failed")
+
             }
         } catch (e: Exception) {
-            println(e.message)
+            throw Exception("Connection failed")
         }
     }
     override fun getAll(): List<TaskEntity> {
-        return listOf()
+        val res = mutableListOf<TaskEntity>()
+        try {
+            val statement = connection?.prepareStatement("SELECT * FROM tasks")
+            val result = statement?.executeQuery()
+            while (result != null && result.next()) {
+                val title = result.getString("title")
+                val desc = result.getString("description")
+                res.add(TaskEntity(title, desc))
+            }
+        } catch (e: Exception) {
+            throw Exception(e)
+        }
+        return res.toList()
     }
 
     override fun addTask(task: TaskEntity) {
-
+        try {
+            val statement = connection?.prepareStatement("SELECT * FROM tasks")
+            val result = statement?.executeQuery()
+            while (result != null && result.next()) {
+                val title = result.getString("title")
+                val desc = result.getString("description")
+                res.add(TaskEntity(title, desc))
+            }
+        } catch (e: Exception) {
+            throw Exception(e)
+        }
     }
 }
